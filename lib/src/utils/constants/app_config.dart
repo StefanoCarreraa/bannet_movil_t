2daba5ca2c16231fca31f06607428f04a2cc966f
Stefano Carrera Alvarado <58115723+StefanoCarreraa@users.noreply.github.com>
2025-02-13 17:59:12 -0500

    ipconfig

--- lib/src/utils/constants/app_config.dart
+++ lib/src/utils/constants/app_config.dart
@@ -1,7 +1,7 @@
 class AppCconfig {
   const AppCconfig._();
 
-  // static const String baseUrl = 'http://movil.bantel.com.pe:5004';
-  static const String baseUrl = 'http://192.168.129.104:5002';
+  static const String baseUrl = 'http://movil.bantel.com.pe:5004';
+  // static const String baseUrl = 'http://192.168.129.104:5002';
   // static const String baseUrl = 'http://192.168.129.200:5002';
 }
